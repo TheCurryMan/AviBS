41105f3c1135910754944fa1ee553168ffe775a1
Avinash Jain <avinashkalpitjain@gmail.com>
2017-05-31 23:23:40 -0700

    Cleaned up code + console output

--- GameController.java
+++ GameController.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 /**
  * 
- *  The main class for the AviBS application
+ * The main class for the AviBS application
  *
- *  @author  Avi, Susan, Harsh
+ * @author Avi, Susan, Harsh
  */
 public class GameController
 {
@@ -17,10 +18,13 @@ public class GameController
 
     Pile currentPile;
 
+
     /**
-     * Constructs a new GameController with an ArrayList of the players who
-     * will be participating in the game. Initializes currentPile to an empty pile. 
-     * @param players the list of players who will be playing in the game
+     * Constructs a new GameController with an ArrayList of the players who will
+     * be participating in the game. Initializes currentPile to an empty pile.
+     * 
+     * @param players
+     *            the list of players who will be playing in the game
      * @author Avi
      */
     public GameController( ArrayList<Player> players )
@@ -29,27 +33,31 @@ public class GameController
         this.currentPile = new Pile();
     }
 
+
     /**
-     * It first checks to see if the player is a human or a bot. If it's a bot, 
+     * It first checks to see if the player is a human or a bot. If it's a bot,
      * Bot's BotPlayCurrentCard method is called and the appropriate response is
-     * printed out. If it's a human player, the player's current hand is first printed
-     * out. The player is then prompted for an input, and they can play either
-     * B for bluff, P for play, or S for skip. If they play B for bluff, then the program 
-     * checks to see if they are correct/if the previous player who played a card 
-     * actually played what they said they did. If so, the player who called the bluff
-     * gets all the cards. If not, the player who played the wrong card gets the pile. 
-     * If the player enters S for skip, the next player in the rotation will be prompted
-     * to play. If a player enters P for play, then they will be prompted for the 
-     * number of cards they want to play and the actual cards in their hand that they 
-     * are going to play. The program also clears the pile if everyone skips, and if 
-     * someone plays/calls a bluff after another player empties their hand, then the 
-     * player with the empty hand wins. 
-     * @param p Player
+     * printed out. If it's a human player, the player's current hand is first
+     * printed out. The player is then prompted for an input, and they can play
+     * either B for bluff, P for play, or S for skip. If they play B for bluff,
+     * then the program checks to see if they are correct/if the previous player
+     * who played a card actually played what they said they did. If so, the
+     * player who called the bluff gets all the cards. If not, the player who
+     * played the wrong card gets the pile. If the player enters S for skip, the
+     * next player in the rotation will be prompted to play. If a player enters
+     * P for play, then they will be prompted for the number of cards they want
+     * to play and the actual cards in their hand that they are going to play.
+     * The program also clears the pile if everyone skips, and if someone
+     * plays/calls a bluff after another player empties their hand, then the
+     * player with the empty hand wins.
+     * 
+     * @param p
+     *            Player
      * @author Avi, Susan
      */
     public void nextTurn( Player p )
     {
-        System.out.println(" ");
+        System.out.println( " " );
         p.sortCards();
         boolean isEmpty = false;
         String name = last.getName();
@@ -122,14 +130,16 @@ public class GameController
                     System.out.println( p.getName() + " skipped" );
                     if ( last.getName().equals( p.getName() ) )
                     {
-              
-                        if (p.getHand().size() == 0)
+
+                        if ( p.getHand().size() == 0 )
                         {
                             isEmpty = true;
                             System.out.println( last.getName() + " won!" );
                             System.out.println( "Thanks for playing!" );
-                        } else {
-                            
+                        }
+                        else
+                        {
+
                             currentPile = new Pile();
                             nextTurn( players.get( 0 ) );
                         }
@@ -144,7 +154,7 @@ public class GameController
             }
             else
             {
-                
+
                 ArrayList<Card> cList = p.BotPlayHighest();
                 type = cList.remove( 0 ).getNum();
                 currentPile.addCards( cList, type );
@@ -170,6 +180,8 @@ public class GameController
         {
             System.out.println( "Hey " + p.getName() + "! It's your turn." );
             System.out.println( "Your current hand: " + p.viewHand() );
+            System.out.println( "There are currently "
+                + currentPile.getNumOfCards() + " cards in the pile" );
             if ( currentPile.hasCurrentCard() )
             {
                 System.out.println(
@@ -185,7 +197,6 @@ public class GameController
             {
                 ArrayList<Card> cList = new ArrayList<Card>();
 
-                
                 System.out.println( "How many cards do you want to play?" );
                 String num = user_input.next();
                 int number = Integer.parseInt( num );
@@ -269,10 +280,14 @@ public class GameController
 
     }
 
+
     /**
-     * This gives each player every third card in the deck. 
-     * @param p player
-     * @param d deck 
+     * This gives each player every third card in the deck.
+     * 
+     * @param p
+     *            player
+     * @param d
+     *            deck
      * @author Harsh
      */
     public static void distribute( ArrayList<Player> p, Deck d )
@@ -286,23 +301,25 @@ public class GameController
 
     }
 
+
     /**
      * The main method for the AviBS application
-     * @param args arguments
+     * 
+     * @param args
+     *            arguments
      * @author Harsh
      */
     public static void main( String[] args )
     {
         System.out.println( "Welcome to AviBS! Let's begin." );
-        
-        Player p1 = new Bot( "Avi" );
+
+        Player p1 = new Human( "Avi" );
         Player p2 = new Bot( "Susan" );
         Player p3 = new Bot( "Harsh" );
-        Player p4 = new Bot( "Bob" );
 
         ArrayList<Player> players = new ArrayList<Player>();
         players.add( p1 );
-        players.add( p4 );
+        players.add( p2 );
         players.add( p3 );
 
         GameController game = new GameController( players );
